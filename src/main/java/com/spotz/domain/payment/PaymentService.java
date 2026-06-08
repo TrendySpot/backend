7d172156bf2b63f4db29e33f98ec4e37174d441c
gold4877 <gold4877@naver.com>
2026-06-08 14:37:48 +0900

    pay/min 머지

--- src/main/java/com/spotz/domain/payment/PaymentService.java
+++ src/main/java/com/spotz/domain/payment/PaymentService.java
@@ -3,8 +3,13 @@ package com.spotz.domain.payment;
 import com.spotz.domain.ticket.Ticket;
 import com.spotz.domain.ticket.TicketRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
